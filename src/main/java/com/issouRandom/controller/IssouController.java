89dceba007735d5d0b9e95cbc021c4404258a6da
Jordan Marques <jordanmarques1994@gmail.com>
2016-09-28 12:51:34 +0200

    working randomizer on icon and wallpaper

---
+++ src/main/java/com/issouRandom/controller/IssouController.java
@@ -0,0 +1,88 @@
+package com.issouRandom.controller;
+
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@RestController
+@RequestMapping("/issou")
+public class IssouController {
+
+
+
+    @ResponseBody
+    @RequestMapping(value = "/wallpaper/random",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getRandomWallpaper() throws IOException {
+
+        Integer randomImageNumber = randomBetween(1, getDirectorySize("images/wallpaper"));
+        return getImages("images/wallpaper/" + randomImageNumber + ".png");
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/wallpaper/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getWallpaperById(@PathVariable("id") String id) throws IOException {
+        return getImages("images/wallpaper/" + id + ".png");
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/icon/random",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getRandomIcon() throws IOException {
+
+        Integer randomImageNumber = randomBetween(1, getDirectorySize("images/icon"));
+        return getImages("images/icon/" + randomImageNumber + ".png");
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/icon/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getById(@PathVariable("id")String id) throws IOException {
+        return getImages("images/icon/" + id + ".png");
+
+    }
+
+
+
+    private Integer getDirectorySize(String path){
+
+        Integer size = 0;
+
+        try {
+            List<String> files = IOUtils.readLines(IssouController.class.getClassLoader()
+                    .getResourceAsStream(path));
+            size = files.size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    private Integer randomBetween(Integer min, Integer max){
+        return  (int)(Math.random() * (max-min)) + min;
+    }
+
+    private byte[] getImages(String path) throws IOException {
+        Resource resource = new ClassPathResource(path);
+        InputStream in = resource.getInputStream();
+
+        return IOUtils.toByteArray(in);
+    }
+
+}
